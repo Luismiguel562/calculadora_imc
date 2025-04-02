587d4e3bc14cee321b19af32712b321a24efec4c
Luis <981624703luis@gmail.com>
2025-04-01 22:03:14 -0300

    alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import {Box} from '@mui/material';
+import { Card, Box, Typography } from '@mui/material';
 
-export default function App () {
+export default function App() {
 
-    return (
-      <Box>
-        TESTE
-      </Box>
-    )
+  return (
+    <Box
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'flex-start'}
+      width={'100vw'}
+      height={'100vh'}
     
-  }
+      bgcolor={'blue'}
+    >
+      <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'colunm',
+        justifycontent: 'center',
+        alignItems: 'center ',
+        widht: '100% ',
+        marginTop: '160px'
+      }}
+      >
+
+        <Box>
+          Calculadora IMC
+        </Box>
+
+        <Box>
+          entradas
+        </Box>
+
+        <Box>
+          resultado
+        </Box>
+
+      </Card>
+    </Box>
+  )
+}
